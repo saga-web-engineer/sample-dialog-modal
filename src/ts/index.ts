27142f6ae8f0d89f8a98ca7a1bb647d31489bcae
kumeyuta <ku8.m.1.e2@gmail.com>
2024-06-12 01:44:07 +0900

    複数モーダル対応

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,40 +1,58 @@
-const openTrigger = document.querySelector<HTMLButtonElement>('button[data-modal-open]');
-const closeTrigger = document.querySelector<HTMLButtonElement>('button[data-modal-close]');
-const modalElement = document.querySelector<HTMLDialogElement>('.modal');
+const openTriggers = document.querySelectorAll<HTMLButtonElement>('button[data-modal-open]');
+const closeTriggers = document.querySelectorAll<HTMLButtonElement>('button[data-modal-close]');
+const modalElements = document.querySelectorAll<HTMLDialogElement>('.modal');
 const scrollbarWidth = window.innerWidth - document.body.clientWidth;
 
-openTrigger?.addEventListener('click', () => {
-  modalElement?.removeAttribute('style');
-  modalElement?.showModal();
-  document.body.style.setProperty('padding-right', `${scrollbarWidth}px`);
-  document.querySelector('.modal__inner')?.scrollTo(0, 0);
+openTriggers.forEach((trigger) => {
+  trigger.addEventListener('click', () => {
+    const modalId = trigger.getAttribute('data-modal-open');
+    const modalElement = Array.from(modalElements).find((el) => el.id === modalId);
+
+    if (modalElement) {
+      modalElement?.removeAttribute('style');
+      modalElement?.showModal();
+      document.body.style.setProperty('padding-right', `${scrollbarWidth}px`);
+      document.querySelector('.modal__inner')?.scrollTo(0, 0);
+    }
+  });
 });
 
-closeTrigger?.addEventListener('click', () => {
-  modalElement?.close();
-  document.body.style.removeProperty('padding-right');
+closeTriggers.forEach((trigger) => {
+  trigger.addEventListener('click', () => {
+    const modalId = (trigger.closest('.modal') as HTMLDialogElement).id;
+    const modalElement = Array.from(modalElements).find((el) => el.id === modalId);
+
+    if (modalElement) {
+      modalElement?.close();
+      document.body.style.removeProperty('padding-right');
+    }
+  });
 });
 
-modalElement?.addEventListener('click', (e) => {
-  const targetElement = e.target as HTMLElement;
-  if (!targetElement.closest('.modal__inner')) {
-    modalElement?.close();
+modalElements.forEach((modal) => {
+  modal.addEventListener('click', (e) => {
+    const targetElement = e.target as HTMLElement;
+    if (!targetElement.closest('.modal__inner')) {
+      modal?.close();
+      document.body.style.removeProperty('padding-right');
+    }
+  });
+
+  modal.addEventListener('close', (e) => {
     document.body.style.removeProperty('padding-right');
-  }
-});
-
-modalElement?.addEventListener('close', (e) => {
-  document.body.style.removeProperty('padding-right');
-  waitDialogAnimation(e.target as HTMLDialogElement)
-    .then(() => {
-      modalElement.style.setProperty('display', 'none');
-    })
-    .catch((error) => {
-      console.error('Error', error); // eslint-disable-line
-    });
+    waitDialogAnimation(e.target as HTMLDialogElement)
+      .then(() => {
+        modal.style.setProperty('display', 'none');
+      })
+      .catch((error) => {
+        console.error('Error', error); // eslint-disable-line
+      });
+  });
 });
 
 const waitDialogAnimation = (dialog: HTMLDialogElement) =>
   Promise.allSettled(
-    Array.from(dialog.getAnimations({ subtree: true })).map((animation) => animation.finished),
+    Array.from(dialog.getAnimations({ subtree: true })).map((animation) => animation.finished)
   );
+
+export {};
